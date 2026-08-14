33c5a0356e8d4a65c84b3086b4398694c11d5666
david <dspiller321@gmail.com>
2026-08-14 16:26:39 +0300

    add dom classlistStyling

---
+++ domClasslistStyling/stsyling.js
@@ -0,0 +1,12 @@
+// function toggleCard(){
+//     const card = document.querySelector('.card')
+//     card.classList.toggle('highlighted')
+// }
+
+
+const div = document.querySelector('#userBadge')
+const role = div.dataset.role
+const id = div.dataset.id
+console.log(role,id)
+div.dataset.status = 'online'
+
